3d18846c9d85b4a4456374253259ec7db65b7548
João Antonio <tonho_kun@ymail.com>
2023-05-23 10:33:35 -0300

    Implementação de Funcs.h

---
+++ include/Funcs.h
@@ -0,0 +1,16 @@
+#ifndef FUNCS_H
+#define FUNCS_H
+
+#include "Musica.h"
+#include "Lista.h"
+
+/**
+ * @brief Entra na primeira opção do menu
+ * 
+*/
+void manageMusics(Lista<Musica *> *musics);
+
+//void managePlaylist();
+
+
+#endif // FUNCS_H
