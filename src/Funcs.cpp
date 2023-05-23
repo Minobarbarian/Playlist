826ed6ff639544086a670d676cbf94a2881aab9a
João Antonio <tonho_kun@ymail.com>
2023-05-23 11:17:17 -0300

    Implementação dos comandos

--- src/Funcs.cpp
+++ src/Funcs.cpp
@@ -65,6 +65,8 @@ void manageMusics(Lista<Musica *> &musics)
   system("clear || cls");
 }
 
+//void editPlaylist
+
 void managePlaylist(Lista<Playlist *> &playlists)
 {
     std::string action;
@@ -73,11 +75,45 @@ void managePlaylist(Lista<Playlist *> &playlists)
         std::cout << "====================-Manage Playlists-===================" << std::endl;
         std::cout << "1) Add new playlist;\n";
         std::cout << "2) Remove playlist;\n";
-        std::cout << "3) List Playlist;\n";
+        std::cout << "3) List Playlists;\n";
         std::cout << "0) Back;\n";
         std::cout << "======================================================" << std::endl;
         std::cout << "Choose your next action: ";
         std::getline(std::cin, action);
+
+        if (action == "1")
+        {
+            std::string playlistName;
+            std::cout << "Insira o nome da playlist que será adicionada:" << std::endl;
+            std::getline(std::cin, playlistName);
+            Playlist *play = new Playlist(playlistName);
+            playlists.inserir(play);
+        }
+
+        if (action == "2")
+        {
+            std::string playlistName;
+            std::cout << "Insira o nome da playlist que será removida:" << std::endl;
+            std::getline(std::cin, playlistName);
+
+            for (int i = 0; i < playlists.tamanho; i++)
+            {
+                if (playlists.busca(i)->dado->getNome().compare(playlistName) == 0)
+                {
+                    playlists.remover(i);
+                    delete playlists.busca(i);
+                }
+            }
+
+        }
+
+        if (action == "3")
+        {
+            for (int i = 0; i < playlists.tamanho; i++)
+            {
+                std::cout << playlists.busca(i)->dado->getNome() << std::endl;
+            }
+        }
 
         if (action == "0") break;
 
